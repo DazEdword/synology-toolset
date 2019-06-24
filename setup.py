b7b15c55bbe7869df5eccae0ea802e5741b8a5f0
Ed <eduardo@gottabegarabi.com>
2019-06-24 18:11:39 +0100

    Added bump version and package files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="synology-toolset",
+    version="0.0.1",
+    author="Ed Garabito",
+    author_email="eduardo@gottabegarabi.com",
+    description="A Python API wrapper and toolset for interfacing with Synology NAS devices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DazEdword/synology-toolset",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
