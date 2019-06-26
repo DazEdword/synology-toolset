b2b7eddb9816932e782aeb4fcb58f1f2044bc2cf
Ed <eduardo@gottabegarabi.com>
2019-06-26 23:41:30 +0100

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="synology-toolset",
-    version="0.1.2",
+    version="0.2.0",
     author="Ed Garabito",
     author_email="eduardo@gottabegarabi.com",
     description="A Python API wrapper and toolset for interfacing with Synology NAS devices.",
