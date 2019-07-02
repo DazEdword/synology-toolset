b34da75fa5ef40b9866c4a1f166e23a2033e9354
Eduardo Garabito <eduardo.garabito@futrli.com>
2019-07-02 14:18:56 +0100

    Bump version: 0.3.11 → 0.3.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="synotools",
-    version="0.3.11",
+    version="0.3.12",
     author="Ed Garabito",
     author_email="eduardo@gottabegarabi.com",
     description="A Python API wrapper and toolset to interact with Synology NAS devices.",
