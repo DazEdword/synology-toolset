e84a164cfbcecc4c9d568f5de903a7cd7ee8b652
Ed <eduardo@gottabegarabi.com>
2019-07-03 21:21:25 +0100

    Replacing travis with buildkite.

--- setup.py
+++ setup.py
@@ -13,11 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DazEdword/synology-toolset",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "fabric",
-        "python-dotenv",
-        "python-synology",
-    ],
+    install_requires=["fabric", "python-dotenv", "python-synology"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
