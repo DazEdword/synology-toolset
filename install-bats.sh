1a7e22a6ecbc6650643dda5d9711d0577a5bdbd9
Ed <eduardo@gottabegarabi.com>
2019-07-06 23:21:56 +0100

    Adjusted bats core installation to work on docker and travis.

--- install-bats.sh
+++ install-bats.sh
@@ -7,19 +7,23 @@ GIT_BRANCH="master"
 set -eo pipefail
 set -u
 
-FORCE=${1:--keep}
+INITIAL_PWD=$PWD
+
+BASE_PATH=${1:-$INITIAL_PWD}
+FORCE=${2:--keep}
 
 function get_bats_repo_and_branch() {
     echo "Current directory: $PWD"
+    echo "Base path: $BASE_PATH"
     FORCE=$1
     PREV_PWD=$PWD
     CHECKOUT_DIR="bats"
 
-    if [ ! -d "$DEPENDENCIES_PATH" ]; then
-        mkdir -p "$DEPENDENCIES_PATH"
+    if [ ! -d "$BASE_PATH/$DEPENDENCIES_PATH" ]; then
+        mkdir -p "$BASE_PATH/$DEPENDENCIES_PATH"
     fi
 
-    cd "$DEPENDENCIES_PATH"
+    cd "$BASE_PATH/$DEPENDENCIES_PATH"
 
     if [[ ! -d "$CHECKOUT_DIR" ]]; then
         echo "Checking out branch $GIT_BRANCH of repo $GIT_REPO ..."
@@ -48,7 +52,10 @@ function pull_repository() {
     cd $PREV_PWD
 }
 
-get_bats_repo_and_branch "$FORCE"
+get_bats_repo_and_branch "$BASE_PATH" "$FORCE"
+
+cd $INITIAL_PWD
+ls -la
 
 STATUS=${?}
 
