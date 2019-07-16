b65c8b7eeb1a10163ac1c82384160bcb6911d30e
Ed <eduardo@gottabegarabi.com>
2019-07-16 19:58:39 +0100

    Improved command checks.

--- synotools/scripts/vpn-check-connection.sh
+++ synotools/scripts/vpn-check-connection.sh
@@ -5,7 +5,12 @@ set -e
 NOT_CONNECTED_MESSAGE="No connection!!"
 
 echo "Checking VPN connection..."
-result=$(synovpnc get_conn || /usr/syno/bin/synovpnc get_conn)
+
+if hash synovpnc 2>/dev/null; then
+	result=$(synovpnc get_conn)
+elif hash /usr/syno/bin/synovpnc 2>/dev/null; then
+	result=$(/usr/syno/bin/synovpnc get_conn)
+fi
 
 if [[ $result == *"$NOT_CONNECTED_MESSAGE"* ]]; then
 	echo "VPN is not connected."
